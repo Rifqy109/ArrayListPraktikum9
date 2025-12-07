5cab0c7dd8d5ad42fae12f29f901cb80179f9c99
Rifqy Kurniawan Fattahillah <2410631170104@student.unsika.ac.id>
2025-12-07 10:51:50 +0700

    Add files via upload

--- src/arraylistpraktikum/ArrayListPraktikum.java
+++ src/arraylistpraktikum/ArrayListPraktikum.java
@@ -3,18 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package arraylistpraktikum;
-
+import java.util.ArrayList;
 /**
  *
  * @author p
  */
 public class ArrayListPraktikum {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+         ArrayList arr = new ArrayList(); 
+        arr.add("Michelle");
+        arr.add("Jonshon");
+        arr.add(20);
+        arr.add(null);
+        arr.set(2, "Arsya");
+       
+        Object removedValue = arr.remove("Michelle");
+        System.out.println("Value yang dihapus: " + removedValue);
+        System.out.println(arr);
+          
+        ArrayList arr1 = new ArrayList();
+        arr1.add("Marcel");
+        arr1.add("Jonathan");
+        arr1.add("Cindy");
+        System.out.println(arr1);
+        
+        System.out.println("Elemen yang diambil: " );
+        //for(int i = 0; i < arr1.size(); i++) {
+        //    System.out.println(arr1.get(i) + " ");
+        //}
+        arr.addAll(arr1);
+        //System.out.println(arr);
+        
+        ArrayList<Integer> nilaiMahasiswa = new ArrayList();
+        nilaiMahasiswa.add(92);
+        nilaiMahasiswa.add(90);
+        nilaiMahasiswa.add(88);
+        nilaiMahasiswa.add(98);
+        System.out.println(nilaiMahasiswa);
+        // nilaiMahasiswa.sort((o1, o2) -> 0);
+        System.out.println(nilaiMahasiswa);
     }
     
 }
